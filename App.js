9c98375944553601d7f296d2e50baa196bd08642
JoaoVitorSil <joaovitorsil13@gmail.com>
2022-10-07 22:10:06 -0300

    Instalação dos pacotes e implementação do Stack Navigator

--- App.js
+++ App.js
@@ -1,100 +1,18 @@
-import { useEffect, useState } from "react";
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, View,FlatList} from 'react-native';
-import Constants from 'expo-constants'
-import { getPokemons, getPokemonData } from './api';
-import Pokemon from './components/Pokemon'
-import Logo from "./styles/logo";
-import Loading from "./components/Loading";
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  const [page, setPage] = useState(0);
-  const [totalPages, setTotalPages] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [pokemons, setPokemons] = useState([]);
-  const itensPerPage = 10;
-  const fetchPokemons = async ()=>{
-    try{
-      setLoading(true)
-      const data = await getPokemons(itensPerPage, itensPerPage * page);
-      const promises = data.results.map(async (pokemon) => {
-        return await getPokemonData(pokemon.url);
-      });
-      const results = await Promise.all(promises)
-      setPokemons(results)
-      setLoading(false);
-      setTotalPages(Math.ceil(data.count / itensPerPage));
-    }
-    catch(error){
-      console.log("fetchPokemons error:", error)
-    }
-  }
-
-  const setPreviousPage = ()=>{
-    if(page > 0) {
-      setPage(page-1)
-  }
-  }
-
-  const setNextPage = ()=>{
-    if(page+1 !== totalPages){
-      setPage(page+1)
-    }
-  }
-  useEffect(()=>{
-    fetchPokemons();
-  },[page])
-  
-  const renderItem = ({ item }) => {
-    //let imgUri = item['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
-    let imgUri = item['sprites']['front_default']
-    return(
-    <Pokemon
-        id = {item.id}
-        name={item.name}
-        srcImg={imgUri}
-        types = {item.types}
-    /> 
-  );}
-  
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-
-      <Logo/>
-      {loading ? (<Loading/>) : 
-      
-      (<FlatList
-        data={pokemons}
-        renderItem={renderItem}
-        keyExtractor = {item => item.id}
-        showsVerticalScrollIndicator={false}
-      />)}
-      
-      <View style={styles.buttons}>
-        <Button 
-          onPress={setPreviousPage}
-          title="Voltar">
-        </Button>
-        <Button 
-          onPress={setNextPage}
-          title="Proxima">
-        </Button>
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    backgroundColor: '#333333',
-    marginTop: Constants.statusBarHeight,
-  },
-  buttons: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    paddingTop: 10,
-    marginBottom: 10,
-  }
-});
