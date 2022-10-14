a3f7aa3d50dd2a2196e85b395553d423f7c22e84
JoaoVitorSil <joaovitorsil13@gmail.com>
2022-10-13 23:21:45 -0300

    Finalização da tela de carregamento e finalização do projeto

--- components/Loading.js
+++ components/Loading.js
@@ -1,7 +1,21 @@
 import React from 'react'
-import { Text } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 export default function Loading() {
   return (
-    <Text>Carregando ...</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Carregando ...</Text>
+    </View>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 40,
+    fontWeight: 'bold',
+  }
+});
